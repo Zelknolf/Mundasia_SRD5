cf12c94d81a161bf864b6d4e1b71f6d041636a76
Zelknolf <zelknolf@yahoo.com>
2016-04-11 18:23:00 -0500

    Command file can be used to create symbolic links to allow the project to be run from debug mode without much manual fuckery.

---
+++ makedebuglinks.cmd
@@ -0,0 +1,17 @@
+cd Mund_SRD5_Client\Mund_SRD5_Client
+md bin
+cd bin
+md Debug
+cd Debug
+mklink /D "DataArrays" "..\..\..\..\_Release\DataArrays"
+mklink /D "Images" "..\..\..\..\_Release\Images"
+mklink /D "TextLibraries" "..\..\..\..\_Release\TextLibraries"
+cd ..\..\..\..
+cd Mund_SRD5_Server\Mund_SRD5_Server
+md bin
+cd bin
+md Debug
+cd Debug
+mklink /D "DataArrays" "..\..\..\..\_Release\DataArrays"
+mklink /D "Images" "..\..\..\..\_Release\Images"
+mklink /D "TextLibraries" "..\..\..\..\_Release\TextLibraries"
