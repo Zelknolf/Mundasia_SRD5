9d452d893b1c107776460b40db84e6f4cfbed345
Zelknolf <zelknolf@yahoo.com>
2016-05-14 13:32:02 -0500

    UI to select races.

--- Mund_SRD5_Client/Mund_SRD5_Client/UI/Screens/CharacterCreationScreen.cs
+++ Mund_SRD5_Client/Mund_SRD5_Client/UI/Screens/CharacterCreationScreen.cs
@@ -45,8 +45,11 @@ namespace Mundasia.Interface
 
         static Panel characterClassIcon = new Panel();
         static Label characterClassText = new Label();
+        static Panel raceIcon = new Panel();
+        static Label raceText = new Label();
 
         static ListView characterClassBox = new ListView();
+        static ListView raceBox = new ListView();
 
         private static bool _eventsInitialized = false;
 
@@ -56,7 +59,7 @@ namespace Mundasia.Interface
             _form.Resize += _form_Resize;
 
             _characterSheet.Height = _form.ClientRectangle.Height - (padding * 2);
-            _characterSheet.Width = (_form.ClientRectangle.Width / 2) - (padding * 3);
+            _characterSheet.Width = (_form.ClientRectangle.Width - (padding * 3)) / 2;
             _characterSheet.Location = new Point(padding, padding);
             _characterSheet.BackColor = Color.Black;
             _characterSheet.BorderStyle = BorderStyle.FixedSingle;
@@ -70,25 +73,40 @@ namespace Mundasia.Interface
             _panel.Size = _form.ClientRectangle.Size;
             _panel.BackColor = Color.Black;
 
+            raceIcon.Size = new Size(64, 64);
+            raceIcon.Location = new Point(padding, padding);
+            raceIcon.BackgroundImage = Race.NullRaceImage;
+
+            raceText.Text = "No Race Selected";
+            raceText.Location = new Point(raceIcon.Location.X + raceIcon.Width + padding, padding);
+            raceText.TextAlign = ContentAlignment.MiddleCenter;
+            StyleLabel(raceText);
+            raceText.Size = new Size((_characterSheet.Width - (raceIcon.Width * 2) - (padding * 5)) / 2, 64);
+
             characterClassIcon.Size = new Size(64, 64);
-            characterClassIcon.Location = new Point(padding, padding);
+            characterClassIcon.Location = new Point(raceText.Location.X + raceText.Width + padding, padding);
             characterClassIcon.BackgroundImage = CharacterClass.NullClassImage;
 
             characterClassText.Text = "No Class Selected";
             characterClassText.Location = new Point(characterClassIcon.Location.X + characterClassIcon.Width + padding, padding);
             characterClassText.TextAlign = ContentAlignment.MiddleCenter;
             StyleLabel(characterClassText);
-            characterClassText.Size = new Size(100, 64);
+            characterClassText.Size = raceText.Size;
 
 
             if (!_eventsInitialized)
             {
+                raceIcon.Click += EditRace;
+                raceText.Click += EditRace;
+                raceBox.ItemSelectionChanged += RaceBox_ItemSelectionChanged;
                 characterClassIcon.Click += EditCharacterClass;
                 characterClassText.Click += EditCharacterClass;
                 characterClassBox.ItemSelectionChanged += CharacterClassBox_ItemSelectionChanged;
                 _eventsInitialized = true;
             }
 
+            _characterSheet.Controls.Add(raceIcon);
+            _characterSheet.Controls.Add(raceText);
             _characterSheet.Controls.Add(characterClassIcon);
             _characterSheet.Controls.Add(characterClassText);
 
@@ -103,6 +121,66 @@ namespace Mundasia.Interface
             _form.Controls.Remove(_panel);
         }
 
+        #region Race Selection
+        private static void RaceBox_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            if (e.IsSelected)
+            {
+                Race selectedRace = Race.GetRace((uint)e.Item.Tag);
+                if (selectedRace != null)
+                {
+                    raceText.Text = e.Item.Name;
+                    raceIcon.BackgroundImage = selectedRace.Icon;
+
+                    _populateRaceList();
+                }
+            }
+        }
+
+        public static void EditRace(object sender, EventArgs e)
+        {
+            if (_currentEdit == CurrentEdit.Race) return;
+
+            _editPanel.Controls.Clear();
+
+            raceBox.Height = _editPanel.Height - (padding * 2);
+            raceBox.Width = _editPanel.Width - (padding * 2);
+            raceBox.Location = new Point(padding, padding);
+            StyleListView(raceBox);
+
+            _populateRaceList();
+
+            _editPanel.Controls.Add(raceBox);
+
+            _currentEdit = CurrentEdit.Race;
+        }
+
+        private static void _populateRaceList()
+        {
+            raceBox.Items.Clear();
+            ImageList imgs = new ImageList();
+            imgs.ImageSize = IconSize;
+            imgs.ColorDepth = ColorDepth.Depth32Bit;
+
+            int imageIndex = 0;
+
+            foreach(Race race in Race.GetRaces())
+            {
+                ListViewItem toAdd = new ListViewItem(new string[] { "", race.Name });
+                toAdd.Name = race.Name;
+                toAdd.ImageIndex = imageIndex;
+                toAdd.Tag = race.Id;
+                toAdd.ToolTipText = StringLibrary.GetString(race.Description);
+                imgs.Images.Add(race.Icon);
+                imageIndex++;
+                StyleListViewItem(toAdd);
+                raceBox.Items.Add(toAdd);
+            }
+            raceBox.SmallImageList = imgs;
+        }
+        #endregion
+
+        #region Character Class Selection
         private static void CharacterClassBox_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
         {
             if(e.IsSelected)
@@ -110,8 +188,17 @@ namespace Mundasia.Interface
                 CharacterClass selectedClass = CharacterClass.GetClass((uint)e.Item.Tag);
                 if(selectedClass != null)
                 {
-                    characterClassText.Text = selectedClass.Name;
+                    characterClassText.Text = e.Item.Name;
                     characterClassIcon.BackgroundImage = selectedClass.Icon;
+
+                    if(selectedClass.SubClassLevel == 1)
+                    {
+                        EditCharacterSubClass(selectedClass);
+                    }
+                    else
+                    {
+                        _populateCharacterClassList();
+                    }
                 }
             }
         }
@@ -120,11 +207,23 @@ namespace Mundasia.Interface
         {
             if (_currentEdit == CurrentEdit.Class) return;
 
+            _editPanel.Controls.Clear();
+
             characterClassBox.Height = _editPanel.Height - (padding * 2);
             characterClassBox.Width = _editPanel.Width - (padding * 2);
             characterClassBox.Location = new Point(padding, padding);
             StyleListView(characterClassBox);
 
+            _populateCharacterClassList();
+
+            _editPanel.Controls.Add(characterClassBox);
+
+            _currentEdit = CurrentEdit.Class;
+        }
+
+        private static void _populateCharacterClassList()
+        {
+            characterClassBox.Items.Clear();
             ImageList imgs = new ImageList();
             imgs.ImageSize = IconSize;
             imgs.ColorDepth = ColorDepth.Depth32Bit;
@@ -136,6 +235,7 @@ namespace Mundasia.Interface
                 if (cl.HitDie > 0) // Subclasses mark themselves as 0 HD.
                 {
                     ListViewItem toAdd = new ListViewItem(new string[] { "", cl.Name });
+                    toAdd.Name = cl.Name;
                     toAdd.ImageIndex = imageIndex;
                     toAdd.Tag = cl.Id;
                     toAdd.ToolTipText = StringLibrary.GetString(cl.Description);
@@ -146,30 +246,64 @@ namespace Mundasia.Interface
                 }
             }
             characterClassBox.SmallImageList = imgs;
-
-            _editPanel.Controls.Add(characterClassBox);
-
-            _currentEdit = CurrentEdit.Class;
         }
+
+        public static void EditCharacterSubClass(CharacterClass selClass)
+        {
+            characterClassBox.Items.Clear();
+            ImageList imgs = new ImageList();
+            imgs.ImageSize = IconSize;
+            imgs.ColorDepth = ColorDepth.Depth32Bit;
+
+            int imageIndex = 0;
+
+            foreach (CharacterClass cl in selClass.SubClasses)
+            {
+                ListViewItem toAdd = new ListViewItem(new string[] { "", selClass.Name + ", " + cl.Name });
+                toAdd.Name = selClass.Name + ", " + cl.Name;
+                toAdd.ImageIndex = imageIndex;
+                toAdd.Tag = cl.Id;
+                toAdd.ToolTipText = StringLibrary.GetString(cl.Description);
+                imgs.Images.Add(cl.Icon);
+                imageIndex++;
+                StyleListViewItem(toAdd);
+                characterClassBox.Items.Add(toAdd);
+            }
+            characterClassBox.SmallImageList = imgs;
+        }
+        #endregion
 
         private static void _form_Resize(object sender, EventArgs e)
         {
             _panel.Size = _form.ClientRectangle.Size;
 
             _characterSheet.Height = _form.ClientRectangle.Height - (padding * 2);
-            _characterSheet.Width = (_form.ClientRectangle.Width / 2) - (padding * 3);
+            _characterSheet.Width = (_form.ClientRectangle.Width - (padding * 3)) / 2;
             _characterSheet.Location = new Point(padding, padding);
 
             _editPanel.Height = _characterSheet.Height;
             _editPanel.Width = _characterSheet.Width;
             _editPanel.Location = new Point(_characterSheet.Location.X + _characterSheet.Width + padding, padding);
 
-            switch(_currentEdit)
+            raceIcon.Location = new Point(padding, padding);
+            raceText.Location = new Point(raceIcon.Location.X + raceIcon.Width + padding, padding);
+            raceText.Size = new Size((_characterSheet.Width - (raceIcon.Width * 2) - (padding * 5)) / 2, 64);
+
+            characterClassIcon.Location = new Point(raceText.Location.X + raceText.Width + padding, padding);
+            characterClassText.Location = new Point(characterClassIcon.Location.X + characterClassIcon.Width + padding, padding);
+            characterClassText.Size = raceText.Size;
+
+            switch (_currentEdit)
             {
                 case CurrentEdit.Class:
                     characterClassBox.Height = _editPanel.Height - (padding * 2);
                     characterClassBox.Width = _editPanel.Width - (padding * 2);
                     characterClassBox.Location = new Point(padding, padding);
+                    break;
+                case CurrentEdit.Race:
+                    raceBox.Height = _editPanel.Height - (padding * 2);
+                    raceBox.Width = _editPanel.Width - (padding * 2);
+                    raceBox.Location = new Point(padding, padding);
                     break;
             }
         }
@@ -179,7 +313,8 @@ namespace Mundasia.Interface
         private enum CurrentEdit
         {
             None,
-            Class
+            Class,
+            Race
         }
 
         private static Font labelFont = new Font(FontFamily.GenericSansSerif, 12.0f);
